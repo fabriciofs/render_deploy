1174ec2c947bab8a4ebf5470b90d4021b0904729
Fabricio Ferreira <fabricio@fabriciofs.eti.br>
2023-04-12 09:42:19 -0300

    Init

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+import fastify from "fastify";
+import { z } from "zod"
+
+const app = fastify()
+
+const prisma = new PrismaClient()
+
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return { users }
+})
+
+app.post('/users', async (request, reply) => {
+  const createUserSchema = z.object({
+    name: z.string(),
+    email: z.string().email()
+  })
+  const { name, email } = createUserSchema.parse(request.body)
+  const user = await prisma.user.create({ data: { name, email } })
+  return reply.status(201).send({ user })
+})
+
+app.listen({
+  host: '0.0.0.0',
+  port: process.env.PORT ? Number(process.env.PORT) : 3333
+}).then((host) => {
+  console.log(`Http Server Running on ${host}`)
+})
